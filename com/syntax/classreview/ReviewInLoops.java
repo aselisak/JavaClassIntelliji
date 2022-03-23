243197a4090919aac0d0271ec748b53240d64cc1
aselisak <kiyatova1984@gmail.com>
2022-03-23 19:24:06 -0400

    2nd commit

---
+++ com/syntax/classreview/ReviewInLoops.java
@@ -0,0 +1,16 @@
+package com.syntax.classreview;
+
+public class ReviewInLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
